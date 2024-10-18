1551105fe9e25c6b89d744e49f7457c372a39402
RL <lojekrafal27@gmail.com>
2024-10-18 08:43:58 +0200

    lekcja 9

---
+++ Lekcja9/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Lekcja9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Samochod samochod1 = new Samochod();
+            Console.WriteLine("Podaj markę samochodu");
+            samochod1.marka = Console.ReadLine();
+            Console.WriteLine("Podaj silnik samochodu");
+            samochod1.silnik = Console.ReadLine();
+            Console.WriteLine("Podaj pojemność samochodu");
+            samochod1.pojemnoscSilnika = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj kolor samochodu");
+            samochod1.kolor = Console.ReadLine();
+            Console.WriteLine("Podaj maksymalną prędkość samochodu");
+            samochod1.maxPredkosc = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj masę samochodu");
+            samochod1.masa = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj moc samochodu");
+            samochod1.mocSilnika = double.Parse(Console.ReadLine());
+            samochod1.WyswietDane();
+
+            Samochod samochod2 = new Samochod();
+            Console.WriteLine("Podaj markę samochodu");
+            samochod2.marka = Console.ReadLine();
+            Console.WriteLine("Podaj silnik samochodu");
+            samochod2.silnik = Console.ReadLine();
+            Console.WriteLine("Podaj pojemność samochodu");
+            samochod2.pojemnoscSilnika = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj kolor samochodu");
+            samochod2.kolor = Console.ReadLine();
+            Console.WriteLine("Podaj maksymalną prędkość samochodu");
+            samochod2.maxPredkosc = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj masę samochodu");
+            samochod2.masa = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj moc samochodu");
+            samochod2.mocSilnika = double.Parse(Console.ReadLine());
+            samochod2.WyswietDane();
+
+        }
+    }
+}
---
+++ Lekcja9/Samochod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lekcja9
+{
+    internal class Samochod
+    {
+        internal string marka;
+        internal string silnik;
+        internal double pojemnoscSilnika;
+        internal string kolor;
+        internal int maxPredkosc;
+        internal double masa;
+        internal double mocSilnika;
+
+        public void WyswietDane()
+        {
+            Console.WriteLine($"Samochód:\n" +
+                $"marka: {marka}\n" +
+                $"typ silnika: {silnik}\n" +
+                $"pojemność: {pojemnoscSilnika}\n" +
+                $"kolor: {kolor}\n" +
+                $"maksymalna prędkość: {maxPredkosc}\n" +
+                $"masa pojazdu: {masa}\n" +
+                $"moc silnika: {mocSilnika}");
+        }
+    }
+}
