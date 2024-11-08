cf171496329e5f466e4967ceb8573df3af45a249
RL <lojekrafal27@gmail.com>
2024-11-08 08:41:16 +0100

    Zadanie 4

--- Lekcja12/obj/Debug/net8.0/Lekcja12.AssemblyInfo.cs
+++ Lekcja12/obj/Debug/net8.0/Lekcja12.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lekcja12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3656507621fab1b84d5c8df7273a919238f862f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219f171ccded297afc800e3d0b67e243b78d4374")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lekcja12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lekcja12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Udostepnione/KonwersjaCnaFiK/obj/Debug/net8.0/KonwersjaCnaFiK.AssemblyInfo.cs
+++ Udostepnione/KonwersjaCnaFiK/obj/Debug/net8.0/KonwersjaCnaFiK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KonwersjaCnaFiK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc80703ffedae72f7c339084b0ef74ea76d70fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("KonwersjaCnaFiK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KonwersjaCnaFiK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
