7a5f0c9bcdd0228e66804f1f97629a7f4c46cf46
RL <lojekrafal27@gmail.com>
2025-05-15 10:22:49 +0200

    Zadanie 3 wpf

---
+++ WPF/Zadanie3/Kalkulator.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Zadanie3
+{
+    public partial class Kalkulator : Window
+    {
+        public Kalkulator()
+        {
+            InitializeComponent();
+        }
+        public static void RunCalculator(string pole, string objetosc, string imgPath)
+        {
+            var k = new Kalkulator();
+            k.wzor_pole.Text = pole;
+            k.wzor_objetosc.Text = objetosc;
+            k.grafika.Source = new BitmapImage(new Uri(imgPath, UriKind.Relative));
+            k.Show();
+        }
+    }
+}
---
+++ WPF/Zadanie3/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie3
+{
+  
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //switch(((Border)sender).Name)
+            //{
+            //    case "prostopadloscian":
+            //        new Prostopadloscian.
+            //        break;
+            //    case "walec":
+            //        Kalkulator.RunCalculator(Walec.pole, Walec.objetosc, Walec.imgPath);
+            //        break;
+            //    case "stozek":
+            //        Kalkulator.RunCalculator(Stozek.pole, Stozek.objetosc, Stozek.imgPath);
+            //        break;
+            //    case "kula":
+            //        Kalkulator.RunCalculator(Kula.pole, Kula.objetosc, Kula.imgPath);
+            //        break;
+            //}
+        }
+    }
+}
